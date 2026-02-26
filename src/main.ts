1850ac7bd24c3efcba4c22f584d185ae38997969
Bashar Eid <bashar.eid@viam.com>
2026-02-26 17:53:23 -0500

    display camera feed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import Cookies from "js-cookie";
 
 let apiKeyId = "";
 let apiKeySecret = "";
+let host = "";
 let machineId = "";
 
 async function main() {
@@ -21,16 +22,55 @@ async function main() {
   if (machine) {
     document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <div>Hello, ${machine.name}!</div>
+      <img id="camera-feed" style="max-width: 100%;" />
     ` 
+  }
+
+  const robotClient = await VIAM.createRobotClient({
+    host: host,
+    credentials: {
+      type: "api-key",
+      payload: apiKeySecret,
+      authEntity: apiKeyId,
+    },
+    signalingAddress: "https://app.viam.com",
+  })
+
+  if (robotClient) {
+    const img = document.getElementById("camera-feed") as HTMLImageElement;
+    const cameraClient = new VIAM.CameraClient(robotClient, "overhead-webcam");
+
+    let prevUrl: string | null = null;
+    
+    while (true) {
+      const images = await cameraClient.getImages();
+
+      if (images.images.length > 0) {
+        const image = images.images[0];
+        const blob = new Blob([image.image as Uint8Array<ArrayBuffer>], { type: image.mimeType });
+        const url = URL.createObjectURL(blob);
+
+        img.src = url;
+
+        if (prevUrl) {
+          URL.revokeObjectURL(prevUrl);
+        }
+
+        prevUrl = url;
+
+        await new Promise((r) => setTimeout(r, 1000));
+      }
+    }
   }
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  // Extract the machine identifier from the URL
   let machineCookieKey = window.location.pathname.split("/")[2];
+ 
   ({
     apiKey: { id: apiKeyId, key: apiKeySecret },
     machineId: machineId,
+    hostname: host,
   } = JSON.parse(Cookies.get(machineCookieKey)!));
 
   main().catch((error) => {
